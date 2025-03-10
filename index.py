7acacae1a089c53c9fa196df37d5729127525aa8
Pedro <ped.vin42cres@gmail.com>
2025-03-10 10:50:34 -0300

    update 10.03

--- index.py
+++ index.py
@@ -7,62 +7,66 @@ import numpy as np
 # Tamanho Layout
 st.set_page_config(layout="wide")
 
-# Origem dos Dados
-df = pd.read_csv("./data/DadosManutencao.csv", delimiter=";")
 
-# Tratamento de Dados
-df["Patrimônio"] = df["Patrimônio"].astype(str)
-df["IdadeExtenso"] = df["IdadeExtenso"].astype(str)
-df["Aquisição"] = pd.to_datetime(df["Aquisição"], format="%d/%m/%Y")
-df["Expiração"] = pd.to_datetime(df["Expiração"], format="%d/%m/%Y")
-df["Ano"] = df["Aquisição"].dt.year.astype(str)
+# Carregamento dos Dados
+def load_data(file_name: str, file_type: str) -> pd.DataFrame:
+    df = pd.read_csv(f"data/{file_name}{file_type}", delimiter=",")
 
-# Transformação de Dados
-df["Categoria"] = df["Idade"].apply(
-    lambda x: "Ótimo" if x <= 5 else ("Regular" if x <= 9 else "Antigo")
-)
-df["Situação"] = np.where(
-    (df["Idade"] >= 9)
-    | ((df["Geração"] <= 6) & (df["CPU"] != "AMD Ryzen 7 3700U"))
-    | (df["Memória"].isin(["04 GB", "06 GB"]))
-    | (df["TipoDisco"] == "HDD"),
-    "Precária",
-    "Satisfatória",
-)
+    # Tratamento dos Dados
+    df["Patrimônio"] = df["Patrimônio"].astype(str)
+    df["IdadeExtenso"] = df["IdadeExtenso"].astype(str)
+    df["Aquisição"] = pd.to_datetime(df["Aquisição"])
+    df["Expiração"] = pd.to_datetime(df["Expiração"])
+
+    # Transformação de Dados
+    df["Ano"] = df["Aquisição"].dt.year.astype(str)
+    df["Categoria"] = df["Idade"].apply(
+        lambda x: "Ótimo" if x <= 5 else ("Regular" if x <= 9 else "Antigo")
+    )
+    df["Situação"] = np.where(
+        (df["Idade"] >= 9)
+        | ((df["Geração"] <= 6) & (df["CPU"] != "AMD Ryzen 7 3700U"))
+        | (df["Memória"].isin(["04 GB", "06 GB"]))
+        | (df["TipoDisco"] == "HDD"),
+        "Precária",
+        "Satisfatória",
+    )
+
+    return df
+
 
 # Segmentação de Dados no Navegador Lateral
-SETORES = ["-"] + list(df["Setor"].unique())
-GARANTIA = ["-"] + list(df["Garantia"].unique())
-TIPOS_DISCO = ["-"] + list(df["TipoDisco"].unique())
-TIPOS_EQUIPAMENTO = ["-"] + list(df["TipoEquipamento"].unique())
+def sidebar_filter(df: pd.DataFrame) -> pd.DataFrame:
+    SETORES = ["-"] + sorted(list(df["Setor"].unique()))
+    GARANTIA = ["-"] + list(df["Garantia"].unique())
+    TIPOS_DISCO = ["-"] + list(df["TipoDisco"].unique())
+    TIPOS_EQUIP = ["-"] + list(df["TipoEquipamento"].unique())
 
-select_setor = st.sidebar.selectbox("Setor", SETORES, index=0)
-select_garantia = st.sidebar.radio("Garantia", GARANTIA, index=0)
-select_tipo_disco = st.sidebar.radio("Tipo de Disco", TIPOS_DISCO, index=0)
-select_tipo_equip = st.sidebar.radio("Tipo de Equipamento", TIPOS_EQUIPAMENTO, index=0)
+    select_setor = st.sidebar.radio("Setor", SETORES, index=0)
+    select_garantia = st.sidebar.radio("Garantia", GARANTIA, index=0)
+    select_tipo_disco = st.sidebar.radio("Tipo de Disco", TIPOS_DISCO, index=0)
+    select_tipo_equip = st.sidebar.radio("Tipo de Equipamento", TIPOS_EQUIP, index=0)
 
-# Aplicando filtros ao DataFrame
-df_filter = df.copy()
+    # Aplicando filtros ao DataFrame
+    df_filter = df.copy()
 
-if select_setor != "-":
-    df_filter = df_filter[df_filter["Setor"] == select_setor]
+    if select_setor != "-":
+        df_filter = df_filter[df_filter["Setor"] == select_setor]
 
-if select_garantia != "-":
-    df_filter = df_filter[df_filter["Garantia"] == select_garantia]
+    if select_garantia != "-":
+        df_filter = df_filter[df_filter["Garantia"] == select_garantia]
 
-if select_tipo_disco != "-":
-    df_filter = df_filter[df_filter["TipoDisco"] == select_tipo_disco]
+    if select_tipo_disco != "-":
+        df_filter = df_filter[df_filter["TipoDisco"] == select_tipo_disco]
 
-if select_tipo_equip != "-":
-    df_filter = df_filter[df_filter["TipoEquipamento"] == select_tipo_equip]
+    if select_tipo_equip != "-":
+        df_filter = df_filter[df_filter["TipoEquipamento"] == select_tipo_equip]
 
-# Medidas
-num_dispositivos = len(df_filter["ServiceTag"])
-count_garantia_ativa = len(df_filter[df_filter["Garantia"] == "Ativa"])
+    return df_filter
 
 
 # Treemap - Dispositivos por Setor
-def sector_treemap():
+def sector_treemap(df_filter: pd.DataFrame):
     fig = px.treemap(
         df_filter["Setor"].str.replace(" ", "<br>"),
         path=["Setor"],
@@ -79,67 +83,81 @@ def sector_treemap():
         ],
     )
     fig.update_traces(root_color="#061953", textinfo="label+value", textfont_size=15)
-    fig.update_layout(margin=dict(t=30, l=0, r=0, b=0), height=250)
+    fig.update_layout(margin=dict(t=30, l=0, r=0, b=0), height=235, title_font_size=18)
     return fig
 
 
 # Indicador - Garantia
-def warranty_indicator():
+def warranty_indicator(df_filter, count_garantia_ativa: int):
+    range_dispositivos = [0, len(df_filter)]
     fig = go.Figure(
         go.Indicator(
             mode="gauge+number",
             value=count_garantia_ativa,
-            title={
-                "text": "Garantia",
-                "font": {
-                    "size": 17,
-                    "weight": 700,
-                },
-            },
             gauge={
                 "axis": {
-                    "range": [None, len(df_filter)],
-                    "tickfont": {"size": 16},
+                    "range": range_dispositivos,
+                    "tickvals": range_dispositivos,
+                    "tickfont": {"size": 20},
+                    "ticks": "",
                 },
                 "bgcolor": "#061953",
                 "bar": {"color": "#0050EB", "thickness": 0.7},
             },
         )
     )
-    fig.update_layout(margin=dict(t=40, l=30, r=30, b=0), height=200)
+    fig.update_layout(
+        margin=dict(t=30, l=30, r=30, b=20),
+        height=190,
+        title={
+            "text": "Garantia",
+            "font": {"size": 18},
+        },
+    )
     return fig
 
 
 # Grafico Colunas - Dispositivos por Ano de Aquisição
-def age_column():
+def age_column(df_filter: pd.DataFrame):
     df_age_category = (
         df_filter.groupby(["Ano", "Categoria"]).size().reset_index(name="Contagem")
     )
-
-    color_map = {"Ótimo": "#ffffff", "Regular": "#66c5f5", "Antigo": "#0078ED"}
 
     fig = px.bar(
         df_age_category,
         title="Computadores Adquiridos",
         x="Ano",
         y="Contagem",
-        labels={"Ano": "", "Contagem": ""},
         text="Contagem",
         color="Categoria",
-        color_discrete_map=color_map,
+        color_discrete_map={
+            "Ótimo": "#ffffff",
+            "Regular": "#66c5f5",
+            "Antigo": "#0078ED",
+        },
         height=350,
     )
     fig.update_traces(textangle=0)
     fig.update_layout(
         margin=dict(t=30, l=0, r=0, b=0),
-        xaxis=dict(type="category"),
-        yaxis=dict(range=[0, max(df_age_category["Contagem"]) + 0.5]),
+        title_font_size=18,
+        font_size=14,
+        xaxis=dict(
+            type="category",
+            title="",
+            tickfont_size=14,
+        ),
+        yaxis=dict(
+            range=[0, max(df_age_category["Contagem"]) + 0.5],
+            title="",
+            tickfont_size=14,
+        ),
     )
     return fig
 
 
 # Gráfico Barras - Dispositivos por Memória e Tipo
-def memory_bar():
+def memory_bar(df_filter: pd.DataFrame):
     df_memoria = (
         df_filter.groupby(["Memória", "TipoMemória"])
         .size()
@@ -148,10 +166,9 @@ def memory_bar():
     fig = px.bar(
         df_memoria,
         title="Memória",
-        y="Memória",
         x="Contagem",
+        y="Memória",
         text="Contagem",
-        labels={"Memória": "", "Contagem": ""},
         color="TipoMemória",
         color_discrete_map={
             "DDR3": "#0050EB",
@@ -162,12 +179,22 @@ def memory_bar():
     )
     fig.update_layout(
         margin=dict(t=30, l=0, r=0, b=0),
-        xaxis=dict(range=[0, df_memoria.groupby(["Memória"]).size() + 1]),
-        yaxis={"categoryorder": "total ascending"},
+        title_font_size=18,
+        font_size=14,
+        xaxis=dict(
+            range=[0, df_memoria.groupby(["Memória"]).size() + 1],
+            title="",
+            tickfont_size=14,
+        ),
+        yaxis=dict(
+            categoryorder="total ascending",
+            title="",
+            tickfont_size=14,
+        ),
         legend=dict(
+            font_size=14,
+            title_text=None,
             orientation="h",
-            yanchor="bottom",
-            y=-0.3,
             xanchor="center",
             x=0.5,
         ),
@@ -176,7 +203,7 @@ def memory_bar():
 
 
 # Gráfico Barras - Dispositivos por Memória e Tipo
-def storage_bar():
+def storage_bar(df_filter: pd.DataFrame):
     df_storage = (
         df_filter.groupby(["Armazenamento", "TipoDisco"])
         .size()
@@ -185,10 +212,9 @@ def storage_bar():
     fig = px.bar(
         df_storage,
         title="Armazenamento",
-        y="Armazenamento",
         x="Contagem",
+        y="Armazenamento",
         text="Contagem",
-        labels={"Armazenamento": "", "Contagem": ""},
         color="TipoDisco",
         color_discrete_map={
             "HDD": "#0050EB",
@@ -199,12 +225,22 @@ def storage_bar():
     )
     fig.update_layout(
         margin=dict(t=30, l=0, r=0, b=0),
-        xaxis=dict(range=[0, df_storage.groupby(["Armazenamento"]).size() + 1]),
-        yaxis={"categoryorder": "total ascending"},
+        title_font_size=18,
+        font_size=14,
+        xaxis=dict(
+            range=[0, df_storage.groupby(["Armazenamento"]).size() + 1],
+            title="",
+            tickfont_size=14,
+        ),
+        yaxis=dict(
+            categoryorder="total ascending",
+            title="",
+            tickfont_size=14,
+        ),
         legend=dict(
+            font_size=14,
+            title_text=None,
             orientation="h",
-            yanchor="bottom",
-            y=-0.3,
             xanchor="center",
             x=0.5,
         ),
@@ -213,34 +249,49 @@ def storage_bar():
 
 
 # Gráfico Barras - Dispositivos por Situação
-def situation_bar():
+def situation_bar(df_filter: pd.DataFrame):
     df_situation = df_filter.groupby(["Situação"]).size().reset_index(name="Contagem")
     fig = px.bar(
         df_situation,
         title="Situação",
-        y="Situação",
         x="Contagem",
+        y="Situação",
         text="Contagem",
-        labels={"Situação": "", "Contagem": ""},
         color="Situação",
         color_discrete_map={
             "Precária": "#0050EB",
             "Satisfatória": "#ffffff",
         },
         orientation="h",
-        height=200,
+        height=190,
     )
     fig.update_layout(
-        margin=dict(t=30, l=0, r=0, b=0),
-        xaxis=dict(range=[0, max(df_situation["Contagem"]) + 1]),
-        yaxis={"categoryorder": "total ascending"},
+        margin=dict(t=30, l=0, r=10, b=0),
+        title_font_size=18,
+        font_size=14,
+        xaxis=dict(
+            range=[0, max(df_situation["Contagem"]) + 1],
+            title="",
+            tickfont_size=14,
+        ),
+        yaxis=dict(
+            categoryorder="total ascending",
+            title="",
+            tickfont_size=14,
+        ),
         showlegend=False,
     )
     return fig
 
 
+# Dados
+df = load_data("DadosPlanilha", ".csv")
+df_filter = sidebar_filter(df)
+num_dispositivos = len(df_filter["ServiceTag"])
+count_garantia_ativa = len(df_filter[df_filter["Garantia"] == "Ativa"])
+
+
 # Dashboard
-# Cabeçaçho
 st.title("Relatório de Dispositivos")
 st.subheader(f"{df['Departamento'].iloc[0]}", divider="blue")
 
@@ -249,7 +300,7 @@ col1a, col1b = col1.columns(2)
 col1a.markdown(
     f"""
     <div style="border: 2px solid #0050eb; border-radius: 10px; margin-bottom: 1rem; padding: 15px;">
-        <h6 style="margin: 0; padding: 3.04px 0; font-size: 14px;"><b><i>Dispositivos</i></b></h6>
+        <h6 style="margin: 0; padding: 3.04px 0; font-size: 16px;"><b><i>Dispositivos</i></b></h6>
         <p style="margin: 0; padding: 0; font-size: 2.25rem; line-height: normal;">{num_dispositivos}</p>
     </div>
     """,
@@ -258,17 +309,17 @@ col1a.markdown(
 col1b.markdown(
     f"""
     <div style="border: 2px solid #0050eb; border-radius: 10px; margin-bottom: 1rem; padding: 15px;">
-        <p style="margin: 0; padding: 3.04px 0; font-size: 14px;"><b><i> </i></b></p>
+        <p style="margin: 0; padding: 3.04px 0; font-size: 16px;"><b><i> </i></b></p>
         <p style="margin: 0; padding: 0; font-size: 2.25rem; line-height: normal;"></p>
     </div>
     """,
     unsafe_allow_html=True,
 )
-col1.plotly_chart(sector_treemap())
-col1.plotly_chart(warranty_indicator())
-col2.plotly_chart(age_column())
+col1.plotly_chart(sector_treemap(df_filter))
+col1.plotly_chart(warranty_indicator(df_filter, count_garantia_ativa))
+col2.plotly_chart(age_column(df_filter))
 
 col2a, col2b = col2.columns(2)
-col2a.plotly_chart(memory_bar())
-col2b.plotly_chart(storage_bar())
-col1.plotly_chart(situation_bar())
+col2a.plotly_chart(memory_bar(df_filter))
+col2b.plotly_chart(storage_bar(df_filter))
+col1.plotly_chart(situation_bar(df_filter))
